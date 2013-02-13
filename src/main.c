951301d2d46ed740da3e4ff1e460f37826f23b00
Louis-Marie Givel <louismariegivel@gmail.com>
2013-02-13 14:17:34 +0100

    Init SDL & screen

--- src/main.c
+++ src/main.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include <SDL/SDL.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 int main(int argc, char* argv[])
 {
+	SDL_Surface *screen = NULL;
 
-	return 0;
+	if(SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		perror("Error initiating SDL.");
+		return EXIT_FAILURE;
+	}
+
+	if((screen = SDL_SetVideoMode(WIDTH, HEIGHT, 0, SDL_HWSURFACE)) == NULL)
+	{
+		perror("Error setting video mode.");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
