acdad9c536c0420775bddb512e39de5dc84fcd37
Louis-Marie Givel <louismariegivel@gmail.com>
2013-02-13 17:32:16 +0100

    Ajout formes plus descente

--- src/main.c
+++ src/main.c
@@ -9,6 +9,11 @@
 #define HEIGHT 600
 //tick in ms
 #define TICK 100
+#define NB_BASE_SHAPE 7
+#define NB_SHAPES 1000
+#define SHAPE_WIDTH 40
+#define GAME_WIDTH 10
+#define GAME_HEIGHT HEIGHT/SHAPE_WIDTH
 
 //allows to manage main loop speed
 void tick()
@@ -29,6 +34,124 @@ void tick()
 	previousTime = time(NULL);
 }
 
+typedef struct shape {
+	SDL_Rect pos;
+	int matrix[4][4];
+} shape;
+
+void blitShape(SDL_Surface* screen, shape* shape, SDL_Surface* sprite, SDL_Rect origin)
+{
+	int x= shape->pos.x*40 + origin.x;
+	int y= shape->pos.y*40 + origin.y;
+	
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
+		{
+			if(shape->matrix[i][j])
+			{
+				SDL_Rect position = {
+					x+40*i,
+					y+40*j};
+				SDL_BlitSurface(sprite, NULL, screen, &position);
+			}
+		}
+	}
+}
+
+void blitScreen(SDL_Surface* screen, int g[GAME_WIDTH][GAME_HEIGHT], SDL_Surface* sprite, SDL_Rect origin)
+{
+	for(int i=0; i<GAME_WIDTH; i++)
+	{
+		for(int j=0; j<GAME_HEIGHT; j++)
+		{
+			if(g[i][j]==1)
+			{
+				SDL_Rect position = {
+					origin.x + i*40,
+					origin.y + j*40};
+				SDL_BlitSurface(sprite, NULL, screen, &position);
+			}
+		}
+	}
+}
+
+void initShape(shape* shape)
+{	
+	int baseShape[7][4][4]= {{
+		{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 1, 1, 1, 1}},
+	    {{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 0, 0, 0, 1},
+		{ 0, 1, 1, 1}},
+	    {{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 0, 0, 1, 1},
+		{ 0, 0, 1, 1}},
+	    {{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 0, 1, 0, 1}},
+	    {{ 0, 0, 0, 0},
+		{ 0, 1, 0, 0},
+		{ 1, 1, 0, 0},
+		{ 0, 1, 0, 0}},
+	    {{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 1, 1, 0, 0},
+		{ 0, 1, 1, 0}},
+	    {{ 0, 0, 0, 0},
+		{ 0, 0, 0, 0},
+		{ 0, 1, 1, 0},
+		{ 1, 1, 0, 0}}};
+	int select = rand()%7;
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
+		{
+			shape->matrix[i][j] = baseShape[select][i][j];
+		}
+	}
+
+	shape->pos.x=0;
+	shape->pos.y=0;
+}
+
+int collision(shape* s, int g[GAME_WIDTH][GAME_HEIGHT])
+{
+	if(s->pos.y+4>=GAME_HEIGHT)
+		return 1;
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
+		{
+			if ((s->matrix[i][j]==1) && (g[s->pos.x+i][s->pos.y+j+1]==1))
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void update(int g[GAME_WIDTH][GAME_HEIGHT], shape* s)
+{
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
+		{
+			if(s->matrix[i][j])
+			{
+				g[s->pos.x+i][s->pos.y+j]=1;
+			}
+		}
+	}
+}
+
+
 int main(int argc, char* argv[])
 {
 	//Init
@@ -47,9 +170,27 @@ int main(int argc, char* argv[])
 	}
 
 	SDL_Surface *background = IMG_Load("../ressources/background.png");
-	
+	SDL_Surface *sprite = IMG_Load("../ressources/shape.png");
+	SDL_Rect origin = {.x=200, .y=0};
+
+	shape shape;
+	initShape(&shape);
+
+	int  game[GAME_WIDTH][GAME_HEIGHT];
+
+	for(int i=0; i<GAME_WIDTH; i++)
+	{
+		for(int j=0; j<GAME_HEIGHT; j++)
+		{
+			game[i][j] = 0;
+		}
+	}
+
+	srand(time(NULL));
+
 	//Main loop
 	int running=1;
+	int cmpt=0;
 	while(running)
 	{
 		SDL_Event event;
@@ -62,16 +203,37 @@ int main(int argc, char* argv[])
 				case SDL_QUIT: 
 					running = 0;
 					break;
+				case SDL_KEYDOWN:
+					break;
 				default:
 					break;
 			}
 		}
 
+		//Game stuff
+		//move shape down
+		if(cmpt>10)
+		{
+			shape.pos.y++;
+			cmpt=0;
+		}
+		else
+		{
+			cmpt++;
+		}
+
+		if(collision(&shape, game))
+		{
+			update(game, &shape);
+			initShape(&shape);
+		}
+
+
 		//Print stuff
 		//Starts with background
 		SDL_BlitSurface(background, NULL, screen, NULL);
-		//for(int i=0; i<NB_
-
+		blitShape(screen, &shape, sprite, origin);
+		blitScreen(screen, game, sprite, origin);
 
 		SDL_Flip(screen);
 		tick();
